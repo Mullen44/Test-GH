8a9e521507e48139893e99a2f404df50be5b1c3e
Mullen44 <65976580+Mullen44@users.noreply.github.com>
2021-11-23 17:55:37 -0800

    Initial Commit

---
+++ Test-GH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-GH
+//
+//  Created by Andrew Mullen on 2021-11-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-GH/Test_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GHApp.swift
+//  Test-GH
+//
+//  Created by Andrew Mullen on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct Test_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
